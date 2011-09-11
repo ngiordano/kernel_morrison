de7ee088e76da103c1a2da9d821b0183249fa4ef
Nelson <ngiordano75@gmail.com>
2011-09-11 14:23:27 -0400

    Automation and MT patch

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "building kernel"
+export ARCH=arm &&
+export CROSS_COMPILE=arm-eabi- &&
+export PATH=$PLATFORM_DIR/prebuilt/linux-x86/toolchain/arm-eabi-4.2.1/bin:$PATH &&
+make ANDROID_BUILD_TOP=$PLATFORM_DIR &&
+make -C $PLATFORM_DIR/system/wlan/bcm/osrc/open-src/src/dhd/linux ANDROID_BUILD_TOP=$PLATFORM_DIR
+
