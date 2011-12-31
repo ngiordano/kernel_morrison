c8f586ebac9005b0401960a8d9a6de574efcd1f7
ngiordano <ngiordano75@gmail.com>
2011-12-31 18:47:36 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,4 +9,5 @@ export ARCH=arm &&
 export CROSS_COMPILE=arm-eabi- &&
 export PATH=$PLATFORM_DIR/prebuilt/linux-x86/toolchain/arm-eabi-4.2.1/bin:$PATH &&
 make ANDROID_BUILD_TOP=$PLATFORM_DIR &&
-make -C $PLATFORM_DIR/system/wlan/bcm/osrc/open-src/src/dhd/linux ANDROID_BUILD_TOP=$PLATFORM_DIR
+make -C $PLATFORM_DIR/system/wlan/bcm/osrc/open-src/src/dhd/linux
+ANDROID_BUILD_TOP=$PLATFORM_DIR
